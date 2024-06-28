49d58bbe8335ec9e767c35894d93ae4906833dd4
Anurag Negi <123005508+Anurag-Negi28@users.noreply.github.com>
2024-06-28 23:10:17 +0530

    Operations such as fill(), size(), empty() performed

--- arrays.cpp
+++ arrays.cpp
@@ -28,5 +28,37 @@ int main()
         cout << num << " ";
     }
 
+    // Accessing the Elements of the array using [] operator
+    //  access first array element
+    cout << numbers[0] << endl; // returns 3
+
+    // access second array element
+    cout << numbers[1] << endl; // returns 2
+
+    // Another way to access the element of the array is to use the at method which checks for out of bound error.
+    cout << numbers.at(0) << endl; // returns 3
+    cout << numbers.at(1) << endl; // returns 2
+    // cout << numbers.at(10) << endl;  throws std::out_of_range exception
+
+    // Using the empty and size method with array
+    cout << "The size of array is: " << numbers.size() << '\n';
+
+    if (numbers.empty())
+    {
+        cout << "The array is empty" << '\n';
+    }
+    else
+    {
+        cout << "The array is not empty" << '\n';
+    }
+
+    // fill the array with zeros
+    numbers2.fill(0);
+
+    cout << "The elements are: ";
+    for (const int num : numbers2)
+    {
+        cout << num << " ";
+    }
     return 0;
 }
