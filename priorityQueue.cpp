cfab098dd8c77e9d1e48e7fbdfaa869ddb10c8b5
Anurag Negi <123005508+Anurag-Negi28@users.noreply.github.com>
2024-07-04 23:48:22 +0530

    Priority Queue

---
+++ priorityQueue.cpp
@@ -0,0 +1,5 @@
+// A priority queue is a special type of queue in which each element is associated with a priority value. And, elements are served on the basis of their priority. That is, higher priority elements are served first.
+// However, if elements with the same priority occur, they are served according to their order in the queue.
+// Assigning Priority Value
+// Generally, the value of the element itself is considered for assigning the priority. For example,
+// The element with the highest value is considered the highest priority element. However, in other cases, we can assume the element with the lowest value as the highest priority element.
